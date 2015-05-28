166604122f70ad43f9a19604d0ec21449c036752
A.S. Lomoff <lomov.as@gmail.com>
2015-05-28 14:49:24 +0300

    fix order of file check in elpers.sh scripty

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -57,7 +57,7 @@ function set_environment_variables {
 }
 
 function unarchive_package {
-  if [ -f ! $build_folder/$full_package_name ]
+  if [ ! -f "$build_folder/$full_package_name" ]
     tar -xzvf $source_folder/$full_package_name.tar.gz -C $build_folder
   fi
 }
